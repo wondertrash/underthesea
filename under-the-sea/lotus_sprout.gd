e6a282c36a4aef634a0ca7ebf92f408f44803896
wondertrash <benqlxie@gmail.com>
2026-02-28 13:03:55 -0800

    font?

--- under-the-sea/lotus_sprout.gd
+++ under-the-sea/lotus_sprout.gd
@@ -1,11 +1,7 @@
 extends Sprite2D
-
-
-# Called when the node enters the scene tree for the first time.
+@onready var lotus_sprout_anim: AnimationPlayer = $AnimationPlayer
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("grow"):
+		lotus_sprout_anim.play("grow")
