01dd1f3db549994edae8c171774bd90c5481121f
wondertrash <benqlxie@gmail.com>
2026-02-28 16:26:18 -0800

    store

--- under-the-sea/resourcestats.gd
+++ under-the-sea/resourcestats.gd
@@ -1,29 +1,29 @@
 extends CanvasLayer
 @onready var player = get_tree().get_first_node_in_group("resourcestats")
-var health_bar: ColorRect
-var hunger_bar: ColorRect
-var health_bg: ColorRect
-var hunger_bg: ColorRect
+var nutrients_bar: ColorRect
+var sunlight_bar: ColorRect
+var nutrients_bg: ColorRect
+var sunlight_bg: ColorRect
 var time_label: Label
 func _ready() -> void:
 	var viewport_size = get_viewport().get_visible_rect().size
-	health_bg = ColorRect.new()
-	health_bg.position = Vector2(10, 10)
-	health_bg.size = Vector2(360, 30)
-	health_bg.color = Color(0.067, 0.0, 0.619, 1.0)
-	add_child(health_bg)
-	health_bar = ColorRect.new()
-	health_bar.position = Vector2(10, 10)
-	health_bar.size = Vector2(360, 30)
-	health_bar.color = Color(0.8, 0.2, 0.2)
-	add_child(health_bar)
-	hunger_bg = ColorRect.new()
-	hunger_bg.position = Vector2(10, 50)
-	hunger_bg.size = Vector2(360, 30)
-	hunger_bg.color = Color(0.2, 0.2, 0.2)
-	add_child(hunger_bg)
-	hunger_bar = ColorRect.new()
-	hunger_bar.position = Vector2(10, 50)
-	hunger_bar.size = Vector2(360, 30)
-	hunger_bar.color = Color(0.8, 0.6, 0.2)
-	add_child(hunger_bar)
+	nutrients_bg = ColorRect.new()
+	nutrients_bg.position = Vector2(10, 10)
+	nutrients_bg.size = Vector2(360, 30)
+	nutrients_bg.color = Color(0.067, 0.0, 0.619, 1.0)
+	add_child(nutrients_bg)
+	nutrients_bar = ColorRect.new()
+	nutrients_bar.position = Vector2(10, 10)
+	nutrients_bar.size = Vector2(360, 30)
+	nutrients_bar.color = Color(0.8, 0.2, 0.2)
+	add_child(nutrients_bar)
+	sunlight_bg = ColorRect.new()
+	sunlight_bg.position = Vector2(10, 50)
+	sunlight_bg.size = Vector2(360, 30)
+	sunlight_bg.color = Color(0.2, 0.2, 0.2)
+	add_child(sunlight_bg)
+	sunlight_bar = ColorRect.new()
+	sunlight_bar.position = Vector2(10, 50)
+	sunlight_bar.size = Vector2(360, 30)
+	sunlight_bar.color = Color(0.8, 0.6, 0.2)
+	add_child(sunlight_bar)
